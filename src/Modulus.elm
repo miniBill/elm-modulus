5c24d2c2d26f740d3069aba064f7c1114837ced4
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-06-19 13:48:34 +0100

    Rename types

--- src/Modulus.elm
+++ src/Modulus.elm
@@ -1,11 +1,66 @@
-module Modulus exposing (D(..), D1(..), D2(..), D3(..), D4(..), D5(..), D6(..), D7(..), D8(..), D9(..), Modulus, d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, modBySafe)
+module Modulus exposing (D(..), D0(..), D1(..), D2(..), D3(..), D4(..), D5(..), D6(..), D7(..), D8(..), Modulus, d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, main, modBySafe)
 
 import Html exposing (Html)
 
 
 main : Html msg
 main =
-    Html.text ""
+    let
+        seventeen : Modulus (D (D () ()) (D () ())) (D1 (D () ()) (D6 () ()))
+        seventeen =
+            d1 <| f7
+    in
+    case modBySafe seventeen 1234 of
+        D0_1 (D0 ()) ->
+            Html.text "0"
+
+        D0_1 (D1 ()) ->
+            Html.text "1"
+
+        D0_1 (D2 ()) ->
+            Html.text "2"
+
+        D0_1 (D3 ()) ->
+            Html.text "3"
+
+        D0_1 (D4 ()) ->
+            Html.text "4"
+
+        D0_1 (D5 ()) ->
+            Html.text "5"
+
+        D0_1 (D6 ()) ->
+            Html.text "6"
+
+        D0_1 (D7 ()) ->
+            Html.text "7"
+
+        D0_1 (D8 ()) ->
+            Html.text "8"
+
+        D0_1 (D9 ()) ->
+            Html.text "9"
+
+        D1_1 (D0_6 ()) ->
+            Html.text "10"
+
+        D1_1 (D1_6 ()) ->
+            Html.text "11"
+
+        D1_1 (D2_6 ()) ->
+            Html.text "12"
+
+        D1_1 (D3_6 ()) ->
+            Html.text "13"
+
+        D1_1 (D4_6 ()) ->
+            Html.text "14"
+
+        D1_1 (D5_6 ()) ->
+            Html.text "15"
+
+        D1_1 (D6_6 ()) ->
+            Html.text "16"
 
 
 type D f p
@@ -21,18 +76,6 @@ type D f p
     | D9 p
 
 
-type D9 f p
-    = D0_9 f
-    | D1_9 f
-    | D2_9 f
-    | D3_9 f
-    | D4_9 f
-    | D5_9 f
-    | D6_9 f
-    | D7_9 f
-    | D8_9 p
-
-
 type D8 f p
     = D0_8 f
     | D1_8 f
@@ -41,7 +84,8 @@ type D8 f p
     | D4_8 f
     | D5_8 f
     | D6_8 f
-    | D7_8 p
+    | D7_8 f
+    | D8_8 p
 
 
 type D7 f p
@@ -51,7 +95,8 @@ type D7 f p
     | D3_7 f
     | D4_7 f
     | D5_7 f
-    | D6_7 p
+    | D6_7 f
+    | D7_7 p
 
 
 type D6 f p
@@ -60,7 +105,8 @@ type D6 f p
     | D2_6 f
     | D3_6 f
     | D4_6 f
-    | D5_6 p
+    | D5_6 f
+    | D6_6 p
 
 
 type D5 f p
@@ -68,29 +114,38 @@ type D5 f p
     | D1_5 f
     | D2_5 f
     | D3_5 f
-    | D4_5 p
+    | D4_5 f
+    | D5_5 p
 
 
 type D4 f p
     = D0_4 f
     | D1_4 f
     | D2_4 f
-    | D3_4 p
+    | D3_4 f
+    | D4_4 p
 
 
 type D3 f p
     = D0_3 f
     | D1_3 f
-    | D2_3 p
+    | D2_3 f
+    | D3_3 p
 
 
 type D2 f p
     = D0_2 f
-    | D1_2 p
+    | D1_2 f
+    | D2_2 p
 
 
 type D1 f p
-    = D0_1 p
+    = D0_1 f
+    | D1_1 p
+
+
+type D0 f p
+    = D0_0 p
 
 
 type Modulus f p
@@ -117,32 +172,48 @@ crash n =
             crash n
 
 
-build1 : Int -> f -> p -> D1 f p
-build1 n f p =
+build1 : Int -> f -> p -> D0 f p
+build1 n _ p =
     case modBy 1 n of
         0 ->
-            D0_1 p
+            D0_0 p
 
         _ ->
             crash n
 
 
-build2 : Int -> f -> p -> D2 f p
+build2 : Int -> f -> p -> D1 f p
 build2 n f p =
     case modBy 2 n of
+        0 ->
+            D0_1 f
+
+        1 ->
+            D1_1 p
+
+        _ ->
+            crash n
+
+
+build3 : Int -> f -> p -> D2 f p
+build3 n f p =
+    case modBy 3 n of
         0 ->
             D0_2 f
 
         1 ->
-            D1_2 p
+            D1_2 f
+
+        2 ->
+            D2_2 p
 
         _ ->
             crash n
 
 
-build3 : Int -> f -> p -> D3 f p
-build3 n f p =
-    case modBy 3 n of
+build4 : Int -> f -> p -> D3 f p
+build4 n f p =
+    case modBy 4 n of
         0 ->
             D0_3 f
 
@@ -150,15 +221,18 @@ build3 n f p =
             D1_3 f
 
         2 ->
-            D2_3 p
+            D2_3 f
+
+        3 ->
+            D3_3 p
 
         _ ->
             crash n
 
 
-build4 : Int -> f -> p -> D4 f p
-build4 n f p =
-    case modBy 4 n of
+build5 : Int -> f -> p -> D4 f p
+build5 n f p =
+    case modBy 5 n of
         0 ->
             D0_4 f
 
@@ -169,15 +243,18 @@ build4 n f p =
             D2_4 f
 
         3 ->
-            D3_4 p
+            D3_4 f
+
+        4 ->
+            D4_4 p
 
         _ ->
             crash n
 
 
-build5 : Int -> f -> p -> D5 f p
-build5 n f p =
-    case modBy 5 n of
+build6 : Int -> f -> p -> D5 f p
+build6 n f p =
+    case modBy 6 n of
         0 ->
             D0_5 f
 
@@ -191,15 +268,18 @@ build5 n f p =
             D3_5 f
 
         4 ->
-            D4_5 p
+            D4_5 f
+
+        5 ->
+            D5_5 p
 
         _ ->
             crash n
 
 
-build6 : Int -> f -> p -> D6 f p
-build6 n f p =
-    case modBy 6 n of
+build7 : Int -> f -> p -> D6 f p
+build7 n f p =
+    case modBy 7 n of
         0 ->
             D0_6 f
 
@@ -216,15 +296,18 @@ build6 n f p =
             D4_6 f
 
         5 ->
-            D5_6 p
+            D5_6 f
+
+        6 ->
+            D6_6 p
 
         _ ->
             crash n
 
 
-build7 : Int -> f -> p -> D7 f p
-build7 n f p =
-    case modBy 7 n of
+build8 : Int -> f -> p -> D7 f p
+build8 n f p =
+    case modBy 8 n of
         0 ->
             D0_7 f
 
@@ -244,15 +327,18 @@ build7 n f p =
             D5_7 f
 
         6 ->
-            D6_7 p
+            D6_7 f
+
+        7 ->
+            D7_7 p
 
         _ ->
             crash n
 
 
-build8 : Int -> f -> p -> D8 f p
-build8 n f p =
-    case modBy 8 n of
+build9 : Int -> f -> p -> D8 f p
+build9 n f p =
+    case modBy 9 n of
         0 ->
             D0_8 f
 
@@ -275,41 +361,10 @@ build8 n f p =
             D6_8 f
 
         7 ->
-            D7_8 p
-
-        _ ->
-            crash n
-
-
-build9 : Int -> f -> p -> D9 f p
-build9 n f p =
-    case modBy 9 n of
-        0 ->
-            D0_9 f
-
-        1 ->
-            D1_9 f
-
-        2 ->
-            D2_9 f
-
-        3 ->
-            D3_9 f
-
-        4 ->
-            D4_9 f
-
-        5 ->
-            D5_9 f
-
-        6 ->
-            D6_9 f
-
-        7 ->
-            D7_9 f
+            D7_8 f
 
         8 ->
-            D8_9 p
+            D8_8 p
 
         _ ->
             crash n
@@ -380,47 +435,47 @@ build modulus builder (Modulus next) =
         }
 
 
-d0 : Modulus f p -> Modulus (D f f) (D1 f p)
+d0 : Modulus f p -> Modulus (D f f) (D0 f p)
 d0 =
     build 0 build1
 
 
-d1 : Modulus f p -> Modulus (D f f) (D2 f p)
+d1 : Modulus f p -> Modulus (D f f) (D1 f p)
 d1 =
     build 1 build2
 
 
-d2 : Modulus f p -> Modulus (D f f) (D3 f p)
+d2 : Modulus f p -> Modulus (D f f) (D2 f p)
 d2 =
     build 2 build3
 
 
-d3 : Modulus f p -> Modulus (D f f) (D4 f p)
+d3 : Modulus f p -> Modulus (D f f) (D3 f p)
 d3 =
     build 3 build4
 
 
-d4 : Modulus f p -> Modulus (D f f) (D5 f p)
+d4 : Modulus f p -> Modulus (D f f) (D4 f p)
 d4 =
     build 4 build5
 
 
-d5 : Modulus f p -> Modulus (D f f) (D6 f p)
+d5 : Modulus f p -> Modulus (D f f) (D5 f p)
 d5 =
     build 5 build6
 
 
-d6 : Modulus f p -> Modulus (D f f) (D7 f p)
+d6 : Modulus f p -> Modulus (D f f) (D6 f p)
 d6 =
     build 6 build7
 
 
-d7 : Modulus f p -> Modulus (D f f) (D8 f p)
+d7 : Modulus f p -> Modulus (D f f) (D7 f p)
 d7 =
     build 7 build8
 
 
-d8 : Modulus f p -> Modulus (D f f) (D9 f p)
+d8 : Modulus f p -> Modulus (D f f) (D8 f p)
 d8 =
     build 8 build9
 
@@ -444,46 +499,46 @@ f0 =
     buildFinal 0 crash
 
 
-f1 : Modulus (D () ()) (D1 () ())
+f1 : Modulus (D () ()) (D0 () ())
 f1 =
     buildFinal 1 build1
 
 
-f2 : Modulus (D () ()) (D2 () ())
+f2 : Modulus (D () ()) (D1 () ())
 f2 =
     buildFinal 2 build2
 
 
-f3 : Modulus (D () ()) (D3 () ())
+f3 : Modulus (D () ()) (D2 () ())
 f3 =
     buildFinal 3 build3
 
 
-f4 : Modulus (D () ()) (D4 () ())
+f4 : Modulus (D () ()) (D3 () ())
 f4 =
     buildFinal 4 build4
 
 
-f5 : Modulus (D () ()) (D5 () ())
+f5 : Modulus (D () ()) (D4 () ())
 f5 =
     buildFinal 5 build5
 
 
-f6 : Modulus (D () ()) (D6 () ())
+f6 : Modulus (D () ()) (D5 () ())
 f6 =
     buildFinal 6 build6
 
 
-f7 : Modulus (D () ()) (D7 () ())
+f7 : Modulus (D () ()) (D6 () ())
 f7 =
     buildFinal 7 build7
 
 
-f8 : Modulus (D () ()) (D8 () ())
+f8 : Modulus (D () ()) (D7 () ())
 f8 =
     buildFinal 8 build8
 
 
-f9 : Modulus (D () ()) (D9 () ())
+f9 : Modulus (D () ()) (D8 () ())
 f9 =
     buildFinal 9 build9
